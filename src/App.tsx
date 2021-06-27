1f0f59b8726c879ebd3deee818afdec728d8fbbc
Cassiano Thurow <cassiano.thurow@gmail.com>
2021-06-27 20:02:46 -0300

    ✨ day 02 finish
    Home, newRoom page / authContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
 
-import './styles/global.scss'
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <Home />
+    <BrowserRouter>
+      <AuthContextProvider>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
